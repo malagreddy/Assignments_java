a30d9b1488dd1c81c0d2626bc44fdb931006e48d
malagreddy <41578465+malagreddy@users.noreply.github.com>
2018-07-26 17:59:47 +0530

    Update testMain.java

--- Inheritance2/src/testMain.java
+++ Inheritance2/src/testMain.java
@@ -1,11 +1,11 @@
-
+//program to find sum and difference of 2 numbers
 public class testMain {
 
 	public static void main(String[] args) {
 		TestClass testclass=new TestClass();
 		
-		System.out.println("sum is :"+ testclass.add());
-		System.out.println("difference is :"+testclass.sub());
+		System.out.println("sum is :"+ testclass.add());//print sum
+		System.out.println("difference is :"+testclass.sub());//print difference
 	}
 
 }
