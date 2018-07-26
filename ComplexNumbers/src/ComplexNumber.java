e4c9f011cc10bf111177001b57f0b97eb5e4bf03
malagreddy <41578465+malagreddy@users.noreply.github.com>
2018-07-26 17:28:14 +0530

    Update ComplexNumber.java

--- ComplexNumbers/src/ComplexNumber.java
+++ ComplexNumbers/src/ComplexNumber.java
@@ -1,6 +1,6 @@
-
+//program to find the sum of two complex numbers
 public class ComplexNumber {
-	private int real;
+	private int real;  //instance members
 	private int imaginary;
 	public ComplexNumber(int real,int imaginary)
 	{
@@ -24,9 +24,10 @@ public class ComplexNumber {
 	public void setImaginary(int imaginary) {
 		this.imaginary = imaginary;
 	}
+	//sum function to find the sum of real and imaginary part
 	public void sum(ComplexNumber complexnum1,ComplexNumber complexnum2)
 	{
-		real=complexnum1.real+complexnum2.real;
+		real=complexnum1.real+complexnum2.real;			
 		imaginary=complexnum1.imaginary+complexnum2.imaginary;
 		display();
 	}
